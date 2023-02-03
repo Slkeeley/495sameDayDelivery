c0bb46e7605570f7cc985a26760f2610eda9e2e1
Slkeeley <83038330+Slkeeley@users.noreply.github.com>
2023-02-03 13:13:51 -0800

    Van can go in reverse

--- Same Day Delivery/Assets/Scripts/CarControls.cs
+++ Same Day Delivery/Assets/Scripts/CarControls.cs
@@ -9,6 +9,8 @@ public class CarControls : MonoBehaviour //THIS SCRIPT IS FOR THE CONTROLS WHILE
     public float rotationSpeed = 100.0f;
     public bool accelerating = false; //check if the van's speed is increasing 
     public bool decellerating = false; //check if the van's speed is decreasing 
+    public bool forwards = false; 
+    public bool backwards = false; 
     public float accelerationSpeed; //the speed at which the car accelerates
     public float decellerationSpeed; //the speed at which the car decellerates
 
@@ -24,10 +26,14 @@ public class CarControls : MonoBehaviour //THIS SCRIPT IS FOR THE CONTROLS WHILE
         drive(); 
         if(decellerating)
         {
-            StartCoroutine(decellerate());
+            if(forwards) StartCoroutine(decellerate());
+            if(backwards) StartCoroutine(decellerateBackwards());
+
         }
     }
 
+
+    //CONTROL INPUTS TO CONTROL THE PLAYER'S VEHICLE 
     void drive()
     {
         /*  if (!accelerating)
@@ -47,6 +53,8 @@ public class CarControls : MonoBehaviour //THIS SCRIPT IS FOR THE CONTROLS WHILE
         if (Input.GetKey(KeyCode.W))
         {
             decellerating = false;
+            forwards = true;
+            backwards = false; 
             StopCoroutine(decellerate());
             if (currSpeed < topSpeed) accelerating = true; 
 
@@ -69,13 +77,48 @@ public class CarControls : MonoBehaviour //THIS SCRIPT IS FOR THE CONTROLS WHILE
                 transform.Translate(0, 0, translation);
             }
         }
-        //if the player lets go of W begin decellerating 
 
-        if (Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.W))  //if the player lets go of W begin decellerating 
         {
             accelerating = false; 
             decellerating = true;
         }
+        //If the player presess S move backward on their relative z axis
+        if (Input.GetKey(KeyCode.S))
+        {
+            decellerating = false;
+            forwards = false; 
+            backwards = true; 
+            StopCoroutine(decellerate());
+            if (currSpeed < topSpeed) accelerating = true;
+
+            if (accelerating)//if the vans current speed is lower than the top speed have it speed up until it reaches the top speed. 
+            {
+                float translation = Input.GetAxis("Vertical") * currSpeed;
+                currSpeed += (accelerationSpeed*0.25f);
+                translation *= Time.deltaTime;
+                transform.Translate(0, 0, translation);
+                if (currSpeed >= topSpeed)
+                {
+                    accelerating = false;
+                    currSpeed = topSpeed;
+                }
+            }
+            else// if the van reaches top speed it is no longer accelerating and have it move at top speed 
+            {
+                float translation = Input.GetAxis("Vertical") * topSpeed;
+                translation *= Time.deltaTime;
+                transform.Translate(0, 0, translation);
+            }
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.S))  //if the player lets go of S begin decellerating Backwards
+        {
+            accelerating = false;
+            decellerating = true;
+        }
+
     }
 
     public IEnumerator decellerate()
@@ -83,6 +126,16 @@ public class CarControls : MonoBehaviour //THIS SCRIPT IS FOR THE CONTROLS WHILE
 
         transform.position += Vector3.forward * currSpeed* Time.deltaTime;
         currSpeed -= decellerationSpeed;
+        yield return new WaitForEndOfFrame();
+        if (currSpeed <= 0) decellerating = false;
+    }
+
+
+    public IEnumerator decellerateBackwards()
+    {
+
+        transform.position += Vector3.back * currSpeed * Time.deltaTime;
+        currSpeed -= (decellerationSpeed*2);
         yield return new WaitForEndOfFrame();
         if (currSpeed <= 0) decellerating = false;
     }
