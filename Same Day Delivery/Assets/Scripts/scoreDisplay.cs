8a567f5c5a14f3376a6ea53f4819e56d3ce3a2ad
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-02-14 16:22:17 -0800

    Upgrades to Next Day Screen

--- Same Day Delivery/Assets/Scripts/scoreDisplay.cs
+++ Same Day Delivery/Assets/Scripts/scoreDisplay.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro; 
 
-public class scoreDisplay : MonoBehaviour
+public class scoreDisplay : MonoBehaviour// this is used to display the players score, packages and 
 {
     public TMP_Text mostRecentScore;
     public TMP_Text packagesDelivered;  
@@ -11,8 +11,13 @@ public class scoreDisplay : MonoBehaviour
     void Start()
     {
         mostRecentScore.text = "Your Score: "+ GameWatcher.currScore.ToString(); 
-        packagesDelivered.text = "Packages Delivered: " + GameWatcher.packagesDelivered.ToString(); 
+        packagesDelivered.text = "Packages Delivered: " + GameWatcher.packagesDelivered.ToString();
+        //put zBucks here
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
+
+
 
 
 }
