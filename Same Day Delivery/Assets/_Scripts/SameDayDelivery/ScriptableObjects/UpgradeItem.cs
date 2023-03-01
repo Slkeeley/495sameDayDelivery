07a7c831d4d56c84a4d241993dcfe9f85f78fbca
Weston Campbell <weston.campbell1@gmail.com>
2023-02-28 16:33:49 -0800

    Working on upgrade infrastructure

---
+++ Same Day Delivery/Assets/_Scripts/SameDayDelivery/ScriptableObjects/UpgradeItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using UnityEngine;
+
+namespace SameDayDelivery.ScriptableObjects
+{
+    [System.Serializable]
+    public class UpgradeValue
+    {
+        public string uName = "";
+        public float uValue;
+    }
+    
+    [CreateAssetMenu(fileName = "UpgradeItem", menuName = "Game/UpgradeItem", order = 51)]
+    public class UpgradeItem : ScriptableObject
+    {
+        public string upgradeName = "zUnknown zUpgrade";
+        [Multiline]
+        public string description = "zotally zawesome zupgrade!";
+        public int cost;
+        public bool purchased;
+        [Header("Value")]
+        public UpgradeValue valueA;
+        public UpgradeValue valueB;
+    }
+}
---
+++ Same Day Delivery/Assets/_Scripts/SameDayDelivery/ScriptableObjects/UpgradeLookupTable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SameDayDelivery.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "UpgradeLookupTable", menuName = "Game/UpgradeLookupTable", order = 51)]
+    public class UpgradeLookupTable : ScriptableObject
+    {
+        public List<UpgradeItem> upgrades = new List<UpgradeItem>();
+    }
+}
