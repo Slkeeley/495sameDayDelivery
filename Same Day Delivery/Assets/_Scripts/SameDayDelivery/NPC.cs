f97113c4b8af0a1d441aa708b7eab7e020248ad6
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-04-06 15:47:13 -0700

    Human Male & Female Ragdolls

--- Same Day Delivery/Assets/_Scripts/SameDayDelivery/NPC.cs
+++ Same Day Delivery/Assets/_Scripts/SameDayDelivery/NPC.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    private Animator am;
-    [SerializeField] private SameDayDelivery.ScriptableObjects.UpgradeItem evilIntentions; 
+    //  private Animator am;
+    [SerializeField] private SameDayDelivery.ScriptableObjects.UpgradeItem evilIntentions;
     [SerializeField] private GameObject ragDoll;
     private SameDayDelivery.Controls.GameWatcher watcher;
     private void Awake()
     {
         ragDoll.SetActive(false);
-        am = GetComponent<Animator>();
-        watcher = GameObject.Find("GameWatcher").GetComponent<SameDayDelivery.Controls.GameWatcher>(); 
+        // am = GetComponent<Animator>();
+        watcher = GameObject.Find("GameWatcher").GetComponent<SameDayDelivery.Controls.GameWatcher>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag=="Van")
+        if (other.tag == "Van" || other.tag =="Player")
         {
-            am.enabled = false;
+          //  am.enabled = false;
             ragDoll.SetActive(true); 
             if(evilIntentions.purchased)
             {
                 watcher.currentScore = watcher.currentScore + 5; 
             }
         }
-    }
 
+        if (other.GetComponent<SameDayDelivery.PackageSystem.Package>())
+        {
+            //  am.enabled = false;
+            ragDoll.SetActive(true);
+            if (evilIntentions.purchased)
+            {
+                watcher.currentScore = watcher.currentScore + 5;
+            }
+        }
+    }
+/*
     private void OnTriggerExit(Collider other)
     {
         if(other.tag=="Van")
@@ -35,5 +45,5 @@ public class NPC : MonoBehaviour
             //reassemble ragdoll 
         }
     }
-
+*/
 }
