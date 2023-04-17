97bd33e2560b6dfc0e580226f38df4f070a9b1c4
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-04-17 14:35:19 -0700

    Street light and car Collisions

--- Same Day Delivery/Assets/StreetLamp.cs
+++ Same Day Delivery/Assets/StreetLamp.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class StreetLamp : MonoBehaviour
 {
     Rigidbody rb;
+    Collider cd; 
+    [SerializeField] private GameObject lamp;
+    [SerializeField] private GameObject lense;
+    [SerializeField] private Material gray; 
+    [SerializeField] private Material lampMat;
+    [SerializeField] private Material transparent;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        cd = GetComponent<BoxCollider>(); 
+        rb.useGravity = false; 
         rb.isKinematic = true; 
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Van")
         {
-            rb.isKinematic = false; 
+            rb.useGravity = true; 
+            rb.isKinematic = false;
+            cd.enabled = false;
+            StartCoroutine(flicker());
+
         }
+    }
+
+    IEnumerator flicker()//changes the material of the lamp from lit to a basic gray after being hit by the van
+    {
+        lense.GetComponent<MeshRenderer>().material = transparent;
+        lamp.GetComponent<MeshRenderer>().material = gray;  
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = lampMat;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = gray;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = lampMat;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = gray;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = lampMat;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = gray;  
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = lampMat;
+        yield return new WaitForSeconds(.1f);
+        lamp.GetComponent<MeshRenderer>().material = gray;
     }
 }
