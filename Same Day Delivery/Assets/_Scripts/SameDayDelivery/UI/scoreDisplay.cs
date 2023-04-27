7e3f22d9c3241a2e0c152d633cecb09173c549dd
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-04-27 15:52:59 -0700

    New Win Screen

--- Same Day Delivery/Assets/_Scripts/SameDayDelivery/UI/scoreDisplay.cs
+++ Same Day Delivery/Assets/_Scripts/SameDayDelivery/UI/scoreDisplay.cs
@@ -7,15 +7,14 @@ namespace SameDayDelivery.UI
     public class scoreDisplay : MonoBehaviour // this is used to display the players score, packages and 
     {
         public TMP_Text mostRecentScore;
-
-        public TMP_Text packagesDelivered;
         public TMP_Text coinsEarned;
+        public static int coinsGained; 
 
         // Start is called before the first frame update
         void Start()
         {
-            mostRecentScore.text = "Your Score: " + GameWatcher.scoreEarned.ToString();
-            packagesDelivered.text = "Packages Delivered: " + GameWatcher.successFullDeliveries.ToString();
+            mostRecentScore.text = "Your <color=#A2A2A2>Z</color>core: " + GameWatcher.scoreEarned.ToString();
+            coinsEarned.text = "Zerg Coin<color=#A2A2A2>z</color> Earned: " + coinsGained.ToString();
 
             
            
